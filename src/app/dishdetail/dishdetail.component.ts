00171060bffdfbb98941b81aa00783f0ae0c92ec
hemasenthilkumar <hema.senthilkumar@outlook.com>
2020-06-28 22:44:53 +0530

    Single page application route params

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {Dish} from '../shared/dish';
+import {Params} from '@angular/router';
+import{Location} from '@angular/common';
+import {DishService} from '../service/dish.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  @Input()
-    dish = Dish;
-  constructor() { }
+ 
+    dish :Dish;
+  constructor(private dishService:DishService, private route:ActivatedRoute,private location: Location) { }
 
   ngOnInit() {
+    let id=this.route.snapshot.params['id'];
+    this.dish=this.dishService.getDish(id);
+  }
+
+  goBack():void
+  {
+    this.location.back();
   }
 
 }
